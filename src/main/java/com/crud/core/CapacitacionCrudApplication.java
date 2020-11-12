bd73620e74afd2d20c7a06a51b2744939d996784
Isaac <ayanegui.isaac@gmail.com>
2020-11-12 10:05:48 -0600

    first commit

---
+++ src/main/java/com/crud/core/CapacitacionCrudApplication.java
@@ -0,0 +1,13 @@
+package com.crud.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacitacionCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacitacionCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/core/CapacitacionCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacitacionCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
